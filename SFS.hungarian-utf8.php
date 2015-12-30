781910d8fbf0a9725841cd7b8675c88a7480359b
Keiro <admin@heimkoma.com>
2015-12-30 14:47:32 -0600

    Add SFS code
    Add SFS code to work on and make changes.

---
+++ SFS.hungarian-utf8.php
@@ -0,0 +1,24 @@
+<?php
+// Version: 0.1
+
+global $helptxt, $txtJs;
+
+//Common
+$txt['sfs_spam_detected'] = 'A(z) %s felhasználó a %s e-mail címmel (IP: %s) egy Spam, kérjük lépjen kapcsolatba a fórum adminisztrátorával.';
+
+//SMF 1.x
+$txt['sfs_txt_sfsenabled'] = 'Stop Forum Spam bekapcsolva';
+$txt['sfs_txt_sfsenabled_desc'] = 'Felhasználó e-mail címének ellenőrzése ha megtalálható a www.stopforumspam.com website-on';
+$txt['sfs_txt_ipcheck'] = 'IP cím ellenőrzése';
+$txt['sfs_txt_ipcheck_desc'] = 'IP cím ellenőrzésének bekapcsolása';
+$txt['sfs_txt_usernamecheck'] = 'Felhasználónév ellenőrzése';
+$txt['sfs_txt_usernamecheck_desc'] = 'Felhasználónév ellenőrzésének bekapcsolása';
+
+//SMF 2.x
+$txt['setting_sfs_enabled'] = 'Stop Forum Spam bekapcsolva';
+$txt['sfs_txt_sfsenabled_desc'] = 'Felhasználó e-mail címének ellenőrzése ha megtalálható a www.stopforumspam.com website-on';
+$txt['sfs_txt_ipcheck'] = 'IP cím ellenőrzése';
+$txt['sfs_txt_ipcheck_desc'] = 'IP cím ellenőrzésének bekapcsolása';
+$txt['sfs_txt_usernamecheck'] = 'Felhasználónév ellenőrzése';
+$txt['sfs_txt_usernamecheck_desc'] = 'Felhasználónév ellenőrzésének bekapcsolása';
+?>
