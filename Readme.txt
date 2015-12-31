af786e71cbf92642f1f18d36bf6eeee0babaa3ed
Keiro <admin@heimkoma.com>
2015-12-30 19:05:45 -0600

    Upload original code for SFS
    Original stuff.

---
+++ Readme.txt
@@ -0,0 +1,46 @@
+Thank you for using Stop Forum Spam Mod.
+This Mod verify the user in the registration page with www.stopfroumspam.com
+is a spam denied the registration.
+
+Uninstall previous version before install this version
+
+Version 1.0
+- Add german language (thanks dom.bn)
+- Add croatian language (thanks Mihac™)
+
+Version 0.9
+- Add hungarian language (thanks robv)
+- Add sfs Icon
+
+Version 0.8
+- Add link check to Stop Forum Spam in Who's on line page
+
+Version 0.7
+- Add french language (thanks teknautomotive)
+- Add greek language (thanks faiza and nikan)
+- Add russian language (thanks Bugo)
+- Add turkish language (thanks PLAYBOY)
+- Add ukrainian language (thanks olex)
+
+Version 0.6
+- Upgrade to SMF 2.0 RC4
+- Add portuguese language (thanks  Joomlamz)
+
+Version 0.5
+- Update language in utf8 encoding
+- Add Arabic language (thanks islam2hamy)
+
+Version 0.4
+- Redirect to Registration Settings Page
+- Enable Check username
+
+Version 0.3
+- use fetch_web_data to get response
+- change some install step
+
+Version 0.2
+- Now can disable/enable the check of IP Address
+
+Version 0.1
+! Release
+
---
+++ SFS.hungarian.php
@@ -0,0 +1,24 @@
+<?php
+// Version: 0.1
+
+global $helptxt, $txtJs;
+
+//Common
+$txt['sfs_spam_detected'] = 'A(z) %s felhaszn&#225;l&#243; a %s e-mail c知mel (IP: %s) egy Spam, k&#233;rj殘 l&#233;pjen kapcsolatba a f&#243;rum adminisztr&#225;tor&#225;val.';
+
+//SMF 1.x
+$txt['sfs_txt_sfsenabled'] = 'Stop Forum Spam bekapcsolva';
+$txt['sfs_txt_sfsenabled_desc'] = 'Felhaszn&#225;l&#243; e-mail c知&#233;nek ellen&#337;rz&#233;se ha megtal&#225;lhat&#243; a www.stopforumspam.com website-on';
+$txt['sfs_txt_ipcheck'] = 'IP c知 ellen&#337;rz&#233;se';
+$txt['sfs_txt_ipcheck_desc'] = 'IP c知 ellen&#337;rz&#233;s&#233;nek bekapcsol&#225;sa';
+$txt['sfs_txt_usernamecheck'] = 'Felhaszn&#225;l&#243;n&#233;v ellen&#337;rz&#233;se';
+$txt['sfs_txt_usernamecheck_desc'] = 'Felhaszn&#225;l&#243;n&#233;v ellen&#337;rz&#233;s&#233;nek bekapcsol&#225;sa';
+
+//SMF 2.x
+$txt['setting_sfs_enabled'] = 'Stop Forum Spam bekapcsolva';
+$txt['sfs_txt_sfsenabled_desc'] = 'Felhaszn&#225;l&#243; e-mail c知&#233;nek ellen&#337;rz&#233;se ha megtal&#225;lhat&#243; a www.stopforumspam.com website-on';
+$txt['sfs_txt_ipcheck'] = 'IP c知 ellen&#337;rz&#233;se';
+$txt['sfs_txt_ipcheck_desc'] = 'IP c知 ellen&#337;rz&#233;s&#233;nek bekapcsol&#225;sa';
+$txt['sfs_txt_usernamecheck'] = 'Felhaszn&#225;l&#243;n&#233;v ellen&#337;rz&#233;se';
+$txt['sfs_txt_usernamecheck_desc'] = 'Felhaszn&#225;l&#243;n&#233;v ellen&#337;rz&#233;s&#233;nek bekapcsol&#225;sa';
+?>
